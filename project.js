3f76200d58037df153ceb5219f85cf56ba3122df
Jake Ashton <jakemaddenashton@gmail.com>
2022-09-09 11:16:16 -0700

    Set up basic todo and project modules

---
+++ project.js
@@ -0,0 +1,7 @@
+import ToDo from 'todo.js';
+
+const Project = (title, description, dueDate, priority) => {
+    const prototype = ToDo(title, description, dueDate, priority);
+    const ToDos = [];
+    return Object.assign({}, {ToDos}, prototype);
+};
---
+++ todo.js
@@ -0,0 +1,10 @@
+const ToDo = (title, description, dueDate, priority) => {
+    return {
+        title: title,
+        description: description,
+        dueDate: dueDate,
+        priority: priority,
+    }
+};
+
+export default ToDo;
